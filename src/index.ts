331be6b1a96ca8770950cc270f28c1028527a0da
rikumiyu <rikumiyu@tencent.com>
2022-06-21 12:52:34 +0800

    fix: export FileUploadStream

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export default RequestBus;
 export { requestRaw } from './request';
 
 export { default as requestAgent } from './agent/request';
-export { default as uploadFileAgent } from './agent/upload-file';
+export { default as uploadFileAgent, FileUploadStream } from './agent/upload-file';
 export { default as downloadFileAgent } from './agent/download-file';
 
 export * from './interfaces';
